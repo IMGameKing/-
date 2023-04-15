ff73762f5004315e60a7665161d03b01d17fa8b1
LiYuRu <gameking780204@gmail.com>
2023-04-15 23:30:12 +0800

    1006註解
    七大數據類型不熟的要查一下

--- JavaScript/1006/app.js
+++ JavaScript/1006/app.js
@@ -1,3 +1,14 @@
+// JavaScript 中有 7 種基本數據類型。這 7 種基本數據類型被稱為primitive data type。
+// Number – 整數與帶小數點的數字。
+// BigInt – 任意長度的整數。
+// String – 字符串。
+// Boolean – true或false兩種值。
+// null – 用來代表某個故意不存在的值。
+// undefined – 未被賦值的變數，就是undefined。
+// symbol – unique identifier。
+// 除了這 7 種基本數據類型之外， JavaScript 的第八種數據類型叫做 object，屬於non-primitive data type。
+// JavaScript 中的 object可能是array, object或是function等等。
+
 // JavaScript是個物件導向的程式語言，所以JavaScript當中的數字可被視為是物件。以下是數字常見可用的methods：
 //  toString() – return一個數字的String。
 //  toFixed(n) – return 被轉換後的數字，到小數點後第n位數。
