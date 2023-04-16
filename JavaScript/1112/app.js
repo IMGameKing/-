95926d0240cdac96164e4eb599723af615eb62eb
LiYuRu <gameking780204@gmail.com>
2023-04-16 14:05:11 +0800

    1112 Math object
    1112 Math object

---
+++ JavaScript/1112/app.js
@@ -0,0 +1,17 @@
+console.log(Math.PI);
+console.log(Math.E);
+
+console.log(Math.pow(3, 2));
+
+for (let i = 0; i < 3; i++) {
+  console.log(Math.random());
+}
+//pseudorandom https://zh.wikipedia.org/zh-tw/%E6%A2%85%E6%A3%AE%E6%97%8B%E8%BD%AC%E7%AE%97%E6%B3%95
+
+console.log(Math.sqrt(2));
+
+console.log(Math.abs(-2));
+
+console.log(Math.floor(9.9999999));
+
+console.log(Math.ceil(9.9999999));
